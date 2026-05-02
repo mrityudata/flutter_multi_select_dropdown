d2119e24fbcbc40702aa507214f3a526a7ee0691
amitchauhan <amitthakursahanpur@gmail.com>
2026-05-03 02:22:40 +0530

    fix :- 1. Fix a bug

--- test/flutter_multiselect_plus_test.dart
+++ test/flutter_multiselect_plus_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:multiselect_dropdown_flutter/flutter_multiselect_plus.dart';
 
 void main() {
   test('adds one to input values', () {
